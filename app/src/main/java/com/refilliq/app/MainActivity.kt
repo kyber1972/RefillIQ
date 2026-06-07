927b1dac360a18a32b250ff53f86d1f714b6f416
kyber1972 <kyber.darth@gmail.com>
2026-06-07 10:40:53 -0700

    v0.1 - First successful launch

---
+++ app/src/main/java/com/refilliq/app/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.refilliq.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.refilliq.app.ui.theme.RefillIQTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            RefillIQTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Greeting(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(
+    modifier: Modifier = Modifier
+) {
+    Scaffold {
+        Text(
+            text = "REFILLIQ FUNCIONA",
+            modifier = Modifier.padding(50.dp)
+        )
+    }
+}
