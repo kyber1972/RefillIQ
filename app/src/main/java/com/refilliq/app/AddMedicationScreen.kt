47926be0f109df14c78e515529db304d8d716d85
kyber1972 <kyber.darth@gmail.com>
2026-08-07 16:12:44 -0700

    feat: observe medications with Room Flow

--- app/src/main/java/com/refilliq/app/AddMedicationScreen.kt
+++ app/src/main/java/com/refilliq/app/AddMedicationScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.LaunchedEffect
